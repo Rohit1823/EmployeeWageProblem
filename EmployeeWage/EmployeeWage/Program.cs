6c934cdcb417175d7c36d490cf37f2f80cb2318c
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-11 18:14:52 +0530

    [Rohit]ADD: Refactor the Code to write a Class Method

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -9,7 +9,8 @@ namespace EmployeeWage
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
+        
+        public static int computeEmpWage()
         {
             Console.WriteLine("Welcome To Employee Wage!");
 
@@ -43,6 +44,11 @@ namespace EmployeeWage
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage : " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
         }
     }
 }
