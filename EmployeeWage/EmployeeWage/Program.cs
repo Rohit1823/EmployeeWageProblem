d7cb44820612b7e9b1d2079fb33bdbe41d275b07
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-11 17:39:01 +0530

    [Rohit]ADD: Calculate Wages till condition of Total Working Hours

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -8,16 +8,18 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage!");
 
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -34,11 +36,12 @@ namespace EmployeeWage
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage : " + empWage);
+                
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays  + "  Employee Hours : " + empHrs );
 
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
