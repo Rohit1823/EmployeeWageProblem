cf5cc73f385714532a0a93a3fec8c16ab46096b1
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-11 17:21:10 +0530

    [Rohit]ADD: Calculate Wages for a Month

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -7,31 +7,39 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage!");
 
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            int totalEmpWage = 0;
 
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                empHrs = 4;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
 
-                case IS_FULL_TIME:
-                empHrs = 8;
-                    break;
-                default:
-                empHrs = 0;
-                    break;           
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
                 Console.WriteLine("Employee Wage : " + empWage);
 
+            }
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
 }
